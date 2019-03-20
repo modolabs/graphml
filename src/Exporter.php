db3a247d9de509763312ca0302fd96905d2d5cbb
Wil Hall <wil@wilhall.com>
2019-03-20 13:09:28 -0400

    Implement attribute saving

--- src/Exporter.php
+++ src/Exporter.php
@@ -4,6 +4,8 @@ namespace Graphp\GraphML;
 
 use Fhaculty\Graph\Exporter\ExporterInterface;
 use Fhaculty\Graph\Graph;
+use JsonSerializable;
+use RuntimeException;
 use SimpleXMLElement;
 use Fhaculty\Graph\Edge\Directed;
 
@@ -18,6 +20,41 @@ class Exporter implements ExporterInterface
      http://graphml.graphdrawing.org/xmlns/1.0/graphml.xsd">
 </graphml>
 EOL;
+
+    const GRAPHML_ATTRIBUTE_TYPE_BOOLEAN = 'boolean';
+    const GRAPHML_ATTRIBUTE_TYPE_INT = 'int';
+    const GRAPHML_ATTRIBUTE_TYPE_LONG = 'long';
+    const GRAPHML_ATTRIBUTE_TYPE_FLOAT = 'float';
+    const GRAPHML_ATTRIBUTE_TYPE_DOUBLE = 'double';
+    const GRAPHML_ATTRIBUTE_TYPE_STRING = 'string';
+
+    const GRAPHML_ATTRIBUTE_FOR_GRAPH = 'graph';
+    const GRAPHML_ATTRIBUTE_FOR_NODE = 'node';
+    const GRAPHML_ATTRIBUTE_FOR_EDGE = 'edge';
+    const GRAPHML_ATTRIBUTE_FOR_ALL = 'all';
+
+    /** @var array GRAPHML_ATTRIBUTE_TYPE_MAP Map PHP scalar types to GraphML attribute types */
+    const GRAPHML_ATTRIBUTE_TYPE_MAP = [
+        'boolean' => self::GRAPHML_ATTRIBUTE_TYPE_BOOLEAN,
+        'integer' => self::GRAPHML_ATTRIBUTE_TYPE_INT,
+        'double' => self::GRAPHML_ATTRIBUTE_TYPE_FLOAT,
+        'string' => self::GRAPHML_ATTRIBUTE_TYPE_STRING,
+
+        // No PHP equivalents
+        // '' => self::GRAPHML_ATTRIBUTE_TYPE_DOUBLE,
+        // '' => self::GRAPHML_ATTRIBUTE_TYPE_DOUBLE,
+        // '' => self::GRAPHML_ATTRIBUTE_TYPE_LONG,
+    ];
+
+    private static function _getGraphMLTypeForAttributeValue($value): string {
+        $phpType = gettype($value);
+        if (!isset(self::GRAPHML_ATTRIBUTE_TYPE_MAP[$phpType])) {
+            kgo_dump($value);
+            throw new RuntimeException("Unable to convert PHP type '{$phpType}' to GraphML type.");
+        }
+
+        return self::GRAPHML_ATTRIBUTE_TYPE_MAP[$phpType];
+    }
 
     /**
      * Exports the given graph instance.
@@ -38,8 +75,7 @@ EOL;
      * This method only supports exporting the basic graph structure, with all
      * vertices and directed and undirected edges.
      *
-     * Note that none of the attributes attached to any objects nor any of the
-     * "advanced concepts" of GraphML (Nested Graphs, Hyperedges and Ports) are
+     * Note that none of the a"advanced concepts" of GraphML (Nested Graphs, Hyperedges and Ports) are
      * currently implemented. We welcome PRs!
      *
      * @param Graph $graph
@@ -52,10 +88,33 @@ EOL;
         $graphElem = $root->addChild('graph');
         $graphElem['edgeDefault'] = 'undirected';
 
+        $attributeDefinitions = [];
+
         foreach ($graph->getVertices()->getMap() as $id => $vertex) {
             /* @var $vertex \Fhaculty\Graph\Vertex */
             $vertexElem = $graphElem->addChild('node');
             $vertexElem['id'] = $id;
+            $vertexElem['label'] = $id;
+
+            $vertexAttributeBag = $vertex->getAttributeBag();
+            foreach ($vertexAttributeBag->getAttributes() as $attributeId => $attributeValue) {
+                if ($attributeValue !== null) {
+                    if ($attributeValue instanceof JsonSerializable) {
+                        $attributeValue = json_encode($attributeValue);
+                    }
+                    $attributeType = self::_getGraphMLTypeForAttributeValue($attributeValue);
+
+                    $dataElem = $vertexElem->addChild('data', $attributeValue);
+                    $dataElem->addAttribute('key', $attributeId);
+
+                    if (!isset($attributeDefinitions[$attributeId])) {
+                        $attributeDefinitions[$attributeId] = [
+                            'for' => self::GRAPHML_ATTRIBUTE_FOR_NODE,
+                            'attr.type' => $attributeType,
+                        ];
+                    }
+                }
+            }
         }
 
         foreach ($graph->getEdges() as $edge) {
@@ -63,10 +122,39 @@ EOL;
             $edgeElem = $graphElem->addChild('edge');
             $edgeElem['source'] = $edge->getVertices()->getVertexFirst()->getId();
             $edgeElem['target'] = $edge->getVertices()->getVertexLast()->getId();
+            $vertexElem['label'] = $edgeElem['source'].' to '. $edgeElem['target'];
 
             if ($edge instanceof Directed) {
                 $edgeElem['directed'] = 'true';
             }
+
+            $edgeAttributeBag = $edge->getAttributeBag();
+            foreach ($edgeAttributeBag->getAttributes() as $attributeId => $attributeValue) {
+                if ($attributeValue !== null) {
+                    if ($attributeValue instanceof JsonSerializable) {
+                        $attributeValue = json_encode($attributeValue);
+                    }
+                    $attributeType = self::_getGraphMLTypeForAttributeValue($attributeValue);
+
+                    $dataElem = $edgeElem->addChild('data', $attributeValue);
+                    $dataElem->addAttribute('key', $attributeId);
+
+                    if (!isset($attributeDefinitions[$attributeId])) {
+                        $attributeDefinitions[$attributeId] = [
+                            'for' => self::GRAPHML_ATTRIBUTE_FOR_EDGE,
+                            'attr.type' => $attributeType,
+                        ];
+                    }
+                }
+            }
+        }
+
+        foreach ($attributeDefinitions as $attributeId => $attributeDefinition) {
+            $attributeElem = $root->addChild('key');
+            $attributeElem['id'] = $attributeId;
+            $attributeElem['attr.name'] = $attributeId; // intentionally the same as `id`
+            $attributeElem['attr.type'] = $attributeDefinition['attr.type'];
+            $attributeElem['for'] = $attributeDefinition['for'];
         }
 
         return $root->asXML();
